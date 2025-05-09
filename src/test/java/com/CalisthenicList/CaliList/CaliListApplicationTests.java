b33a1c6690dd7504afe18898483bfdf905b6adf6
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2025-05-09 17:24:26 +0200

    back end start

---
+++ src/test/java/com/CalisthenicList/CaliList/CaliListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CalisthenicList.CaliList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

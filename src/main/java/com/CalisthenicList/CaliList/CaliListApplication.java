523244fed761cde233deea4d82df2ce1e128d34e
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2025-05-09 17:24:26 +0200

    back end start

---
+++ src/main/java/com/CalisthenicList/CaliList/CaliListApplication.java
@@ -0,0 +1,13 @@
+package com.CalisthenicList.CaliList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaliListApplication.class, args);
+    }
+
+}

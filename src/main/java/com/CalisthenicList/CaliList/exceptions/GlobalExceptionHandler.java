7f32829b970d94697518fa68ea1a09cfe180ac47
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2025-06-05 14:42:50 +0200

    login endpoint

--- src/main/java/com/CalisthenicList/CaliList/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/CalisthenicList/CaliList/exceptions/GlobalExceptionHandler.java
@@ -10,17 +10,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        logger.log(Level.WARNING, ex.getMessage(), ex);
         List<String> errors = ex.getBindingResult().getFieldErrors()
                 .stream().map(FieldError::getDefaultMessage)
                 .collect(Collectors.toList());
         return new ResponseEntity<>(getErrorsMap(errors), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, List<String>>> handleUnpredictedExceptions(Exception ex) {
+        logger.log(Level.SEVERE, ex.getMessage(), ex);
+        List<String> errors = List.of(
+                "An unexpected error occurred",
+                "Message: " + ex.getMessage());
+        return new ResponseEntity<>(getErrorsMap(errors), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private Map<String, List<String>> getErrorsMap(List<String> errors) {

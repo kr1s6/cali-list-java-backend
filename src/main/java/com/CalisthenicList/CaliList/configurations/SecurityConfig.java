036af1d1aabb075d811e3b32c66b02ba62ea63f6
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2025-09-18 16:18:21 +0200

    update

--- src/main/java/com/CalisthenicList/CaliList/configurations/SecurityConfig.java
+++ src/main/java/com/CalisthenicList/CaliList/configurations/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.CalisthenicList.CaliList.filter.UserValidationRateLimitingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,8 +28,6 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http, UserValidationRateLimitingFilter userValidationRateLimitingFilter)
 			throws Exception {
 		return http
-				// Disable CSRF for simplicity and allow all requests
-				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
 				// Register the rate-limiting filter before the UsernamePasswordAuthenticationFilter
 				.addFilterBefore(userValidationRateLimitingFilter, UsernamePasswordAuthenticationFilter.class)
